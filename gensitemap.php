87a093de1d98f188a61889a6731d3214b7b867e2
Kieran <kieran@harkin.me>
2016-05-05 16:42:59 +0100

    rename url

--- gensitemap.php
+++ gensitemap.php
@@ -2,6 +2,7 @@
 	require_once('sitemap-php/Sitemap.php');
 	require_once('db.php');
 
+	$sitemap = new Sitemap('https://example.com');
 	$sitemap->setPath('sitemap/');
 	$sitemap->addItem('/', '1.0');
 
@@ -13,4 +14,5 @@
 		$sitemap->addItem($url, '0.8', 'daily');
 	}
 
+	$sitemap->createSitemapIndex('https://example.com/sitemap/', 'Today');
 ?>
