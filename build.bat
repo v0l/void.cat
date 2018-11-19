75675c0a87dd7c3d6274bff81f08ebf54bf844fd
Kieran <kieran@harkin.me>
2018-11-20 01:26:03 +0800

    update for release

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-sass src/style.scss dist/style.css && google-closure-compiler src/script.js src/ripemd160.js --js_output_file dist/script.min.js --language_out ECMASCRIPT_NEXT
+sass src/css/style.scss dist/style.css && google-closure-compiler src/js/script.js --js_output_file dist/script.min.js --language_out ECMASCRIPT_NEXT
