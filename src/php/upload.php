0f96b26f31de8c5d72fa7a6bf322dee136a67542
Kieran <kieran@harkin.me>
2019-03-29 23:07:36 +0800

    this was missing?

--- src/php/upload.php
+++ src/php/upload.php
@@ -28,7 +28,8 @@
         public function HandleRequest() : void {
             if(isset($_SERVER["HTTP_ORIGIN"])) {
                 header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
-                header("Access-Control-Allow-Method: POST");
+                header("Access-Control-Allow-Method: POST,OPTIONS");
+				header("Access-Control-Allow-Headers: Content-Type");
             }
 
             $rsp = new UploadResponse();
