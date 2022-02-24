e496c06296ceb8a05f9f4734700c27584309253c
Kieran <kieran@harkin.me>
2022-02-24 13:58:15 +0000

    Add footer links

--- VoidCat/spa/src/FooterLinks.js
+++ VoidCat/spa/src/FooterLinks.js
@@ -1,9 +1,12 @@
 import "./FooterLinks.css"
+import StrikeLogo from "./image/strike.png";
 
 export function FooterLinks(props){
     return (
         <div className="footer">
             <a href="https://discord.gg/8BkxTGs" target="_blank">Discord</a>
+            <a href="https://invite.strike.me/KS0FYF" target="_blank">Get Strike <img src={StrikeLogo} alt="Strike logo"/> </a>
+            <a href="https://github.com/v0l/void.cat" target="_blank">GitHub</a>
         </div>
     );
 }
