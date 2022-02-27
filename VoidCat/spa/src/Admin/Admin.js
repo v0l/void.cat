061adad404de484bda5b6f2e92543f99e7ecafd0
Kieran <kieran@harkin.me>
2022-02-27 21:52:44 +0000

    Cleanup admin page

--- VoidCat/spa/src/Admin/Admin.js
+++ VoidCat/spa/src/Admin/Admin.js
@@ -1,23 +1,17 @@
-﻿import {useDispatch, useSelector} from "react-redux";
+﻿import "./Admin.css";
+import {useSelector} from "react-redux";
 import {FileList} from "./FileList";
 import {UserList} from "./UserList";
-
-import "./Admin.css";
-import {logout} from "../LoginState";
 import {Navigate} from "react-router-dom";
 
 export function Admin() {
     const auth = useSelector((state) => state.login.jwt);
-    const dispatch = useDispatch();
-    
+
     if (!auth) {
         return <Navigate to="/login"/>;
     } else {
         return (
             <div className="admin">
-                <h2>Admin</h2>
-                <button onClick={() => dispatch(logout())}>Logout</button>
-                
                 <h4>Users</h4>
                 <UserList/>
 
