57dc0aadd7bd3272f3122ab724f192df358e9fb2
Kieran <kieran@harkin.me>
2022-03-08 22:15:56 +0000

    Add build info

--- VoidCat/spa/src/GlobalStats.js
+++ VoidCat/spa/src/GlobalStats.js
@@ -1,9 +1,10 @@
-﻿import {useEffect, useState} from "react";
+﻿import {Fragment, useEffect, useState} from "react";
 import FeatherIcon from "feather-icons-react";
 import {FormatBytes} from "./Util";
 
 import "./GlobalStats.css";
 import {useApi} from "./Api";
+import moment from "moment";
 
 export function GlobalStats(props) {
     const {Api} = useApi();
@@ -19,23 +20,30 @@ export function GlobalStats(props) {
     useEffect(() => loadStats(), []);
 
     return (
-        <dl className="stats">
-            <div>
-                <FeatherIcon icon="upload-cloud"/>
-                {FormatBytes(stats?.bandwidth?.ingress ?? 0, 2)}
-            </div>
-            <div>
-                <FeatherIcon icon="download-cloud"/>
-                {FormatBytes(stats?.bandwidth?.egress ?? 0, 2)}
-            </div>
-            <div>
-                <FeatherIcon icon="hard-drive"/>
-                {FormatBytes(stats?.totalBytes ?? 0, 2)}
-            </div>
-            <div>
-                <FeatherIcon icon="hash"/>
-                {stats?.count ?? 0}
-            </div>
-        </dl>
+        <Fragment>
+            <dl className="stats">
+                <div>
+                    <FeatherIcon icon="upload-cloud"/>
+                    {FormatBytes(stats?.bandwidth?.ingress ?? 0, 2)}
+                </div>
+                <div>
+                    <FeatherIcon icon="download-cloud"/>
+                    {FormatBytes(stats?.bandwidth?.egress ?? 0, 2)}
+                </div>
+                <div>
+                    <FeatherIcon icon="hard-drive"/>
+                    {FormatBytes(stats?.totalBytes ?? 0, 2)}
+                </div>
+                <div>
+                    <FeatherIcon icon="hash"/>
+                    {stats?.count ?? 0}
+                </div>
+            </dl>
+            {stats.buildInfo ? <div className="build-info">
+                {stats.buildInfo.version}-{stats.buildInfo.gitHash}
+                <br/>
+                {moment(stats.buildInfo.buildTime).fromNow()}
+            </div> : null}
+        </Fragment>
     );
 }
